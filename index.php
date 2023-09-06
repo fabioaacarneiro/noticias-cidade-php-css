994d3ecc3216a5ad98dce504d4358341c4fdb0ec
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-09-06 20:57:54 -0300

    adicionado logo e ajustado background do site

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Notícias Cidade</title>
+</head>
+
+<body>
+    <div id="container">
+        <div id="top">
+            <h1 class="logo">Notícias Cidade</h1>
+        </div>
+    </div>
+</body>
+
+</html>
