a8d59b855d34f9d419ffc393b3599c4063c6797c
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-09-06 21:59:06 -0300

    Criado navbar com estilo

--- index.php
+++ index.php
@@ -8,10 +8,19 @@
     <title>Notícias Cidade</title>
 </head>
 
-<body>
+<body class="home">
     <div id="container">
         <div id="top">
             <h1 class="logo">Notícias Cidade</h1>
+            <ul id="navbar">
+                <li class="first"><a id="home" href="index.php">Home</a></li>
+                <li><a id="brasil" href="brasil.php">Brasil</a></li>
+                <li><a id="internacional" href="internacional.php">Internacional</a></li>
+                <li><a id="economia" href="economia.php">Economia</a></li>
+                <li><a id="saude" href="saude.php">Saúde</a></li>
+                <li><a id="ciencia" href="ciencia.php">Ciência</a></li>
+                <li><a id="fotos" href="fotos.php">Fotos</a></li>
+            </ul>
         </div>
     </div>
 </body>
