f839e84cebc791f0760cff8e1f9ec260ca9b0cae
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-09-08 11:06:24 -0300

    ajustado css para aparência do menu lateral

--- index.php
+++ index.php
@@ -49,12 +49,30 @@
                 <div class="box">
                     <h2>Entrevistas</h2>
                     <div class="box-content">
-                        Conteúdo<br>
-                        Conteúdo<br>
-                        Conteúdo<br>
-                        Conteúdo<br>
-                        Conteúdo<br>
-                        Conteúdo
+                        <ul>
+                            <li><a href="">José Almeida</a></li>
+                            <li><a href="">Felipe Silva</a></li>
+                            <li><a href="">Renato Rodrigues</a></li>
+                            <li><a href="">Abelardo Silveira</a></li>
+                            <li><a href="">André Carlos</a></li>
+                            <li><a href="">Márcio Mello</a></li>
+                            <li><a href="">João Pedro</a></li>
+                            <li><a href="">Fernando Weiss</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="box">
+                    <h2>News</h2>
+                    <div class="box-content">
+                        <form action="">
+                            <div>
+                                <label for="email">Email:</label>
+                                <input type="text" name="email" id="email" placeholder="E-mail">
+                            </div>
+                            <div>
+                                <input class="submit" type="submit" value="Cadastrar">
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
